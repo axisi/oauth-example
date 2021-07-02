1aa12d3e624761dc4a9984b044ce79138fc4398a
Grzegorz Macała <gmacala92@outlook.com>
2021-07-02 13:34:53 +0200

    working 3rd commit

--- src/main/java/com/example/oauthexample/oauthexample/OauthExampleApplication.java
+++ src/main/java/com/example/oauthexample/oauthexample/OauthExampleApplication.java
@@ -24,18 +24,20 @@ public class OauthExampleApplication extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(a -> a
-                    .antMatchers("/","/error","webjars/**").permitAll()
+                    .antMatchers("/","/error","/webjars/**").permitAll()
                     .anyRequest().authenticated()
                 )
                 .exceptionHandling(e -> e
                     .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                ).logout(l -> l
+                    .logoutSuccessUrl("/").permitAll()
                 )
                 .oauth2Login();
     }
 
     @GetMapping("/user")
     public Map<String, Object> user (@AuthenticationPrincipal OAuth2User principal){
-        return Collections.singletonMap("name",principal.getAttribute("name"));
+        return Collections.singletonMap("name",principal.getAttribute("login"));
     }
 
     public static void main(String[] args) {
